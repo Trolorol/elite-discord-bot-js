2078526931dc8ac304dbcfb2736c14e7b33d10a4
MiguelSaraiva <47040343+MiguelSaraiva@users.noreply.github.com>
2021-02-27 16:33:21 +0000

    apaga canais e vai buscar os membros dentro do convívio

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv').config();
 const config = require('./config.json');
 
 const client = new Discord.Client();
+var tempchannel = [];
+var connectedusers = [];
 
 const prefix = "?";
 
@@ -13,30 +15,47 @@ client.once('ready', () => {
 });
 const categoryID = '815064384010453014'
 client.on('ready', () =>{
-
-    client.on('message', (msg) => {
-        if (msg.content === '?mm')  msg.guild.channels.create("name", {
-            type: 'voice', 
-        })
-        .then((channel) => {
-            channel.setParent(categoryID);
-        })
-      });
-
-    /*
-    command(client, 'createvoicechannel', (message) =>{
-        
-        const name = message.content.replace('?mm ' , '')
-        
-        message.guild.channels.create(name, {
-            type: 'voice', 
-        })
     
-        .then((channel) => {})
-    })*/
+    client.on('message', (msg) => {
+        if (msg.content === '?mm')
+        {
+            createChannel(msg);
+        } 
+        if (msg.content === '?d') 
+        {
+            deleteChannel(msg);
+        }
 
+        if (msg.content === '?c')
+        {
+            getConnectedUsers(msg, client);
+        }
+    });
 })
 
 
+function createChannel(msg)
+{
+    msg.guild.channels.create("canal de teste", {
+        type: 'voice', 
+    })
+    
+    .then((channel) => {
+        channel.setParent(categoryID);
+        tempchannel.push(channel);
+    })
+}
+
+function deleteChannel(msg)
+{
+    tempchannel.pop().delete();
+}
+
+function getConnectedUsers(msg, client)
+{
+    const channelID = '521629727136546817';
+
+    console.log(client.channels.cache.get(channelID).members);
+}
 
 client.login(process.env.BOT_LOGIN);
