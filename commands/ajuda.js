46272d2005a730da2dd9315e732601f6a512bd3f
André Oliveira <andre@outlook.pt>
2021-03-02 22:11:26 +0000

    Adicionado comando de --ajuda --testes

---
+++ commands/ajuda.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'ajuda',
+    aliases: [],
+    execute(message, args,client,result) {
+        return "\n Os comandos do BOT são os seguintes: \n **--ping** --> Pinga o Bot \n **--testes** --> Link dos testes/trabalhos dos cursos";
+    }
+}
