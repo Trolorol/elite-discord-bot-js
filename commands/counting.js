09c60cc84f6caab315589192a609679265dabf59
João Calapez <joao.calapez.c@gmail.com>
2021-03-04 01:38:13 +0000

    Count gaming tested and working

--- commands/counting.js
+++ commands/counting.js
@@ -1,73 +1,58 @@
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 module.exports = {
     name: 'counting',
     aliases: [],
-    execute(message, args, client, result) {
-        return "blabla";
-    }
-
-}
-
-//message objeto toal 
-//args array de argumentos depois do comando 
-// client bot object 
-// result 
-
-
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-let count = 0
-let max_counting = []
-let last_clients_array = []
-
-const csvWriter = createCsvWriter({
-  path: './database/count.csv',
-  header: [
-      {id: 'id_jogo', title: 'Jogo'},
-      {id: 'date', title:'Data'},
-      {id: 'loser', title: 'Perdedor'},
-      {id: 'debt', title: 'Dividas'},
-  ]
-});
+    execute(message, channel, member, args, client, result) {
+        return'';
+    },
 
 
 
+countingGame(channel, message, member, client) {
 
-client.on('message', ({channel, content, member}) => {
   // Only do this for the counting channel of course
   // If you want to simply make this work for all channels called 'counting', you
   // could use this line:
   // if (client.channels.cache.filter(c => c.name === 'counting').keyArray().includes(channel.id))
-  if (channel.id === client.channels.cache.find(r=>{r.name == 'counting'}).keys()[0]) {
-        
-    if (Number(content) === count + 1 && typeof last_clients_array != 'undefined' && member.user.id != last_clients_array[last_clients_array.length-1] ) {// If the message is the current count + 1...
-      count++
-      last_clients_array.push(member.user.id)
-    } else {
-      var unique = last_clients_array.slice(last_clients_array.length-20,last_clients_array.length).filter(onlyUnique);
-      unique.forEach( (item, i, self) => self[i] = '<@item>');
-      
-      channel.send(`${member} lixou a contagem, estás a dever jolas aos seguintes membros:${unique.toString()}`).catch(console.error)
+  if (channel.id === client.env.count_room_id) {
 
-      count = 0
+    if (Number(+message.content) === client.env.count + 1 && typeof client.env.last_clients_array != 'undefined' && member.user.id != client.env.last_clients_array[client.env.last_clients_array.length-1] ) {// If the message is the current count + 1...
+      client.env.count++
+      client.env.last_clients_array.push(member.user.id)
+      message.react('✅')
+
+    } else {
+      var unique = client.env.last_clients_array.slice(((client.env.last_clients_array.length-20<0)?0:client.env.last_clients_array.length-20),client.env.last_clients_array.length).filter(this.onlyUnique).filter(r => r!=member.user.id)
+      unique.forEach( (item, i, self) => self[i] = `<@${item}>`);
+
+      message.react('🤬')
+      if (unique.length==0){
+        channel.send(`${member} lixou a contagem, não há ninguem para pagar jolas, estás mesmo a tentar jogar sozinho?`)
+      }else{
+        channel.send(`${member} lixou a contagem, estás a dever jolas aos seguintes membros:${unique.toString()}`).catch(console.error)
+      }
+      client.env.count = 0
+      client.env.last_clients_array = []
       const records = [
-        {id_jogo: 'Bob',  date: new Date().toISOString().slice(0,19), loser:member.user.id, debt:unique}
+        {date: new Date().toISOString().slice(0,19), loser:member.user.id, debt:JSON.stringify(unique)}
       ];
-      csvWriter.writeRecords(records)       // returns a promise
-       .then(() => {
-        console.log('...Done');
+      const csvWriter = createCsvWriter({
+        path: './database/count.csv',
+        header: [
+            {id: 'date', title:'Data'},
+            {id: 'loser', title: 'Perdedor'},
+            {id: 'debt', title: 'Dividas'},
+        ],
+        append:true
       });
+      csvWriter.writeRecords(records) // returns a promise
     }
   }
-})
+},
 
-function onlyUnique(value, index, self) {
+ onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
 
 
-
-
-
-
-
-
-
+}
--- index.js
+++ index.js
@@ -2,22 +2,38 @@ const Discord = require('discord.js');
 const dotenv = require('dotenv').config();
 const config = require('./config.json');
 const fs = require('fs');
-
+const csv = require('csv-parser');
 const client = new Discord.Client();
+const counting_game = require('./commands/counting.js')
 client.commands = new Discord.Collection();
 
 client.env={
-    "PREFIX":"--",
+    "PREFIX":"%",
     "TESTE_CATEGORY_ID":'815064384010453014',
     "CONVIVIO_VOICE_CHANNEL_ID":"521629727136546817",
     "tempchannel":[],
-    "connectedusers":[]
+    "connectedusers":[],
+    "count_room_id":0,
+    "count":0,
+    "last_clients_array":[]
 }
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith(".js"));
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name.toLowerCase(), command);
+}
+
+client.database = {}
+const databaseFiles = fs.readdirSync('./database/').filter(file => file.endsWith(".csv"));
+for (const file of databaseFiles) {
+    fs.createReadStream("./database/"+file).pipe(csv())
+    .on('data', (row) => {
+        if(typeof client.database[file.split(".")[0]] == "undefined"){
+            client.database[file.split(".")[0]]=[];
+        }
+        client.database[file.split(".")[0]].push(row);
+    });
 }
 
 client.once('ready', () => {
@@ -28,11 +44,13 @@ client.once('ready', () => {
 client.on('ready', () =>{
     
     client.on('message', (msg) => {
+        if (msg.author.bot) return;
+        console.log(Number.isInteger(+msg.content))
         if(msg.content.startsWith(client.env["PREFIX"])){
             const args = msg.content.slice(client.env["PREFIX"].length).split(/ +/);
             const commandName = args.shift().toLowerCase();
             const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-            var result;
+            var result = "";
             if (command != undefined) {
                 try {
                      result = command.execute(msg, msg.channel, msg.member, args, client, result);
@@ -40,15 +58,18 @@ client.on('ready', () =>{
                     console.log("Command "+commandName+" failed to execute due to:" + error);
                 }
             } else {
-                //games / other commands
+                 result = 'Command not found'
             }
           
             if(result !=""){
                 
                 msg.channel.send(result);
             }
-        } else if (channel.id === client.channels.cache.find(r=>{r.name == 'counting'}).keys()[0] && !isNaN(Number(msg.content))) {
-            result = counting.execute(msg, msg.channel, msg.member, args, client);
+            
+        } else if (Number.isInteger(+msg.content) && client.env.count_room_id == msg.channel.id) {
+            console.log("Entrei no counting")
+            console.log(msg.content)
+            counting_game.countingGame(msg.channel, msg, msg.member, client)
         }
     });
 });
